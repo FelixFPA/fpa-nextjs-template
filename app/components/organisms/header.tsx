ec001198e2aa843e4137f562047aead9fd1aeb81
Felix Horn <felix.horn@fisherpaykel.com>
2023-06-26 14:04:53 +1200

    homepage more setup

--- app/components/organisms/header.tsx
+++ app/components/organisms/header.tsx
@@ -1,13 +1,27 @@
 
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Spacer } from '@chakra-ui/react';
 
 const Header = () => {
   return (
-    <Box as="header" w="100%" p={4} bgColor="#005AAB">
-      <Text fontSize="xl" color="white">
-        Haier
-      </Text>
+    <Box>
+
+      <Box as="header" w="100%" p={4} bgColor="#005AAB">
+        <Text fontSize="xl" color="white">
+          Haier
+        </Text>
+      </Box>
+      <Box as="header" w="100%" p={4} bgColor="#FFFFFF">
+        <Flex>
+          <Spacer></Spacer>
+          <Box>Single Room</Box>
+          <Box>Multi Room</Box>
+          <Box>Whole Home</Box>
+          <Box>More</Box>
+          <Spacer></Spacer>
+        </Flex>
+      </Box>
     </Box>
+
   );
 };
 
