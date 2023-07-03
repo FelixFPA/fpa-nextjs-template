50c7908fd4541606d545aa872a267e15d9ebe63d
Felix Horn <felix.horn@fisherpaykel.com>
2023-07-03 22:00:27 +1200

    strapi publish

--- frontend/app/components/organisms/Homepage.tsx
+++ frontend/app/components/organisms/Homepage.tsx
@@ -1,8 +1,19 @@
 
 import Hero from "../molecules/Hero";
 
+async function getData() {
+    const res = await fetch('http://localhost:1337/api/homepage' )
+    res = res.data.attributes
 
-
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+console.log(getData())
+  
 const Homepage = () => {
     return (
         <div>
@@ -14,7 +25,7 @@ const Homepage = () => {
           description="Home heating and cooling that is efficient, climate friendly and connected."
           buttonText="Get a quote"
         /> 
-
+        
         </div>
 
     );
