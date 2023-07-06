44e877a7804910b09128eac3ad9954672570c3ed
Felix Horn <felix.horn@fisherpaykel.com>
2023-07-06 15:27:06 +1200

    create Controller component

--- frontend/app/components/organisms/Homepage.tsx
+++ frontend/app/components/organisms/Homepage.tsx
@@ -1,18 +1,7 @@
 
 import Hero from "../molecules/Hero";
 
-async function getData() {
-    const res = await fetch('http://localhost:1337/api/homepage' )
-    res = res.data.attributes
 
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
-    }
-   
-    return res.json()
-  }
-console.log(getData())
   
 const Homepage = () => {
     return (
